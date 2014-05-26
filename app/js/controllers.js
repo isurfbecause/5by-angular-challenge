b50f9d0a4f83a5649055f7728d992d646029d6ce
winston_yee <winsyee@gmail.com>
2014-05-26 12:48:33 -0700

    reformatted code and added formating to date

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,38 +9,35 @@ angular.module('myApp.controllers', ['LocalStorageModule'])
 
    // localStorageService.clearAll();
 
-    $scope.displayVideos = function(){
+  $scope.displayVideos = function(){
+    var videos = [];
 
-      var videos = [];
-        for (var key in localStorage){
-           videos.push( localStorageService.get( key.split('.')[1] ) );
-        }
+    for (var key in localStorage){
+      videos.push( localStorageService.get( key.split('.')[1] ) );
+    }
 
-         $scope.videos = videos;
-      }
-
-      $scope.displayVideos();
-
+    $scope.videos = videos;
+  }
 
   $scope.getMeta   = function( youtubeURL ){
     youtubeService.events( youtubeURL )
+
     .success(function(data, status, headers) { // the success function wraps
 
-          $scope.vidId = data.items[0].id.videoId;
-          $scope.result = data.items[0].snippet;
+      var vidId     = data.items[0].id.videoId;
+      $scope.result = data.items[0].snippet;
 
-          localStorageService.set($scope.vidId, $scope.result);
+      localStorageService.set(vidId, $scope.result);
 
-          $scope.displayVideos();
+      $scope.displayVideos();
 
-        })
+    })
 
     .error(function(data, status, headers, config) {
         console.log( 'error' );
     });
-
-
-
   }
+
+  $scope.displayVideos();
 
 }]);
