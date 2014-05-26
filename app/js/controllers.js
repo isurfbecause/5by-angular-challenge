beb7b9b8e8e775425c7d684bed356cd788ba1b77
winston_yee <winsyee@gmail.com>
2014-05-26 13:05:08 -0700

    reformatted js with 4 spaces

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,39 +5,42 @@
 angular.module('myApp.controllers', ['LocalStorageModule'])
 
 .controller('HomeCtrl', ['$scope','youtubeService','localStorageService', function($scope, youtubeService, localStorageService) {
-  $scope.message = 'The 5by Challenge!';
 
-   // localStorageService.clearAll();
+    $scope.message = 'The 5by Challenge!';
 
-  $scope.displayVideos = function(){
-    var videos = [];
+    // localStorageService.clearAll();
 
-    for (var key in localStorage){
-      videos.push( localStorageService.get( key.split('.')[1] ) );
+    $scope.displayVideos = function(){
+        var videos = [];
+
+        //Loop through storage keys and add to videos array
+        for (var key in localStorage){
+            videos.push( localStorageService.get( key.split('.')[1] ) );
+        }
+
+        $scope.videos = videos;
+     }
+
+    //Look up youtube meta data
+    $scope.getMeta   = function( youtubeURL ){
+         youtubeService.events( youtubeURL )
+
+        .success(function(data, status, headers) { // the success function wraps
+
+            var vidId     = data.items[0].id.videoId;
+            $scope.result = data.items[0].snippet;
+
+            localStorageService.set(vidId, $scope.result);
+
+            $scope.displayVideos();
+
+        })
+
+        .error(function(data, status, headers, config) {
+            console.log( 'error' );
+        });
     }
 
-    $scope.videos = videos;
-  }
-
-  $scope.getMeta   = function( youtubeURL ){
-    youtubeService.events( youtubeURL )
-
-    .success(function(data, status, headers) { // the success function wraps
-
-      var vidId     = data.items[0].id.videoId;
-      $scope.result = data.items[0].snippet;
-
-      localStorageService.set(vidId, $scope.result);
-
-      $scope.displayVideos();
-
-    })
-
-    .error(function(data, status, headers, config) {
-        console.log( 'error' );
-    });
-  }
-
-  $scope.displayVideos();
+    $scope.displayVideos();
 
 }]);
