eadd2acdb84a50d86d15c86b2a4cba91f5ad3cd7
winston_yee <winsyee@gmail.com>
2014-05-25 18:02:44 -0700

    Now using local storage

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.controllers', 'myApp.filters', 'myApp.services', 'myApp.directives'])
+angular.module('myApp', ['myApp.controllers', 'myApp.filters', 'myApp.services', 'myApp.directives', 'LocalStorageModule'])
 
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/', {
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,20 +2,45 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
+angular.module('myApp.controllers', ['LocalStorageModule'])
 
-.controller('HomeCtrl', ['$scope','youtubeService', function($scope, youtubeService) {
+.controller('HomeCtrl', ['$scope','youtubeService','localStorageService', function($scope, youtubeService, localStorageService) {
   $scope.message = 'The 5by Challenge!';
+
+   // localStorageService.clearAll();
+
+    $scope.displayVideos = function(){
+
+      var videos = [];
+        for (var key in localStorage){
+           videos.push( localStorageService.get( key.split('.')[1] ) );
+        }
+
+         $scope.videos = videos;
+      }
+
+      $scope.displayVideos();
+
 
   $scope.getMeta   = function( youtubeURL ){
     youtubeService.events( youtubeURL )
     .success(function(data, status, headers) { // the success function wraps
+
+          $scope.vidId = data.items[0].id.videoId;
           $scope.result = data.items[0].snippet;
+
+          localStorageService.set($scope.vidId, $scope.result);
+
+          $scope.displayVideos();
+
         })
 
     .error(function(data, status, headers, config) {
         console.log( 'error' );
     });
+
+
+
   }
 
 }]);
